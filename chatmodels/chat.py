6fd5f9569f4c5db8b7a98d9b0c7ce571ab6b72ad
Mdfaiza007 <145556402+Mdfaiza007@users.noreply.github.com>
2026-09-15 17:37:59 +0530

    Initial commit

---
+++ chatmodels/chat.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+from httpx import HTTPStatusError
+
+load_dotenv()  # Load environment variables from .env file
+
+# from langchain.chat_models import init_chat_model
+# from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain.chat_models import init_chat_model
+from langchain_groq import ChatGroq
+
+from langchain_mistralai import ChatMistralAI
+
+# model = init_chat_model("google_genai:gemini-3.7-flash")
+# model = ChatGoogleGenerativeAI(	model="gemini-3.7-flash")
+# model =   init_chat_model("groq:openai/gpt-oss-120b")
+model = ChatGroq(model="openai/gpt-oss-120b", temperature=0.9, max_tokens=300)
+
+# model = ChatMistralAI(model = "labs-leanstral-1-5-1", temperature=0)
+
+
+response = model.invoke("write a paragraph for job crisis ?")
+
+print(response.content)
